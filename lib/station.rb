821616badcedc474241b717cd133504c6934bbbc
Misa Ogura <misa.ogura01@gmail.com>
2016-03-10 15:37:19 +0000

    Added some more stations to the list

--- lib/station.rb
+++ lib/station.rb
@@ -2,13 +2,15 @@ class Station
   LIST = {
           holborn: 1,
           algate: 1,
-          turnpike_lane: 3,
           arsenal: 2,
+          kilburn: 2,
+          turnpike_lane: 3,
           wimbledon: 3,
           richmond: 4,
-          kew_garden: 4
+          kew_garden: 4,
+          hounslow_west: 5,
+          heathrow_Terminals: 6
           }
-
 
   attr_reader :zone, :name
 
